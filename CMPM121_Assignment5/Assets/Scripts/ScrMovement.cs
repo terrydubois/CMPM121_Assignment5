9071e4b690aa573b9fe5e9b4133fd8e71cb006e8
Terry DuBois <36257165+terrydubois@users.noreply.github.com>
2019-02-14 18:41:50 -0800

    Basic Movement

---
+++ CMPM121_Assignment5/Assets/Scripts/ScrMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Suriyun {
+    public class ScrMovement : MonoBehaviour
+    {
+        public float moveSpeed;
+        private float rot = 0;
+        public float rotSpeed = 80;
+        public float gravity = 8;
+
+        Vector3 moveDir = Vector3.zero;
+
+        CharacterController controller;
+        Animator animator;
+
+        void Start()
+        {
+            controller = GetComponent<CharacterController>();
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            if (controller.isGrounded) {
+                if (Input.GetKey(KeyCode.W)) {
+                    moveDir = new Vector3(0, 0, 1);
+                    moveDir *= moveSpeed;
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                if (Input.GetKeyUp(KeyCode.W)) {
+                    moveDir = Vector3.zero;
+                }
+            }
+
+            rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, rot, 0);
+
+            moveDir.y -= gravity * Time.deltaTime;
+            controller.Move(moveDir * Time.deltaTime);
+        }
+    }
+}
