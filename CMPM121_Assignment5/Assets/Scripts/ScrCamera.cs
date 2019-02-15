d608ff4d37ecced8304f97e60bc6b1e69a7841de
Terry DuBois <tdubois@ucsc.edu>
2019-02-15 01:01:23 -0800

    Basic Character Switching

---
+++ CMPM121_Assignment5/Assets/Scripts/ScrCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Suriyun {
+    public class ScrCamera : MonoBehaviour
+    {
+        static int characterAmount = 2;
+        public GameObject[] characters = new GameObject[characterAmount];
+        private int currentCharacter = 0;
+
+        // https://answers.unity.com/questions/600577/camera-rotation-around-player-while-following.html
+        public float turnSpeed = 4.0f;
+        public Transform playerTransform;
+        private Vector3 offset;
+
+        void Start()
+        {
+            offset = new Vector3(playerTransform.position.x, playerTransform.position.y + 0.2f, playerTransform.position.z + 2.5f);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Z)) {
+                if (currentCharacter < characterAmount - 1) {
+                    currentCharacter++;
+                }
+                else {
+                    currentCharacter = 0;
+                }
+            }
+
+            for (int i = 0; i < characterAmount; i++) {
+                characters[i].GetComponent<ScrMovement>().selected = false;
+            }
+            characters[currentCharacter].GetComponent<ScrMovement>().selected = true;
+            playerTransform = characters[currentCharacter].transform;
+
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            transform.position = playerTransform.position + offset;
+            transform.LookAt(playerTransform.position);
+        }
+    }
+}
--- CMPM121_Assignment5/Assets/Scripts/ScrMovement.cs
+++ CMPM121_Assignment5/Assets/Scripts/ScrMovement.cs
@@ -11,51 +11,69 @@ namespace Suriyun {
         public float rotSpeed = 80;
         public float gravity = 8;
 
+        public bool selected = false;
+
         Vector3 moveDir = Vector3.zero;
 
         CharacterController controller;
         Animator animator;
-        private Rigidbody rb;
+        //Animation animation;
 
         void Start()
         {
             controller = GetComponent<CharacterController>();
             animator = GetComponent<Animator>();
-            rb = GetComponent<Rigidbody>();
+            //animation = GetComponent<Animation>();
         }
 
         void Update()
         {
-        
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-                animator.SetInteger("animation", 1);
-                moveDir = new Vector3(0, 0, 1);
-                moveDir *= moveSpeed;
+
+            if (selected && controller.isGrounded) {
+                // walk forward
+                if (Input.GetKey(KeyCode.UpArrow)) {
+                    animator.SetInteger("animation", 1);
+                    moveDir = new Vector3(0, 0, 1);
+                    moveDir *= moveSpeed;
+                    
+                    // run if player is holding X
+                    if (Input.GetKey(KeyCode.X)) {
+                        moveDir *= 2;
+                        animator.SetInteger("animation", 2);
+                    }
+
+                    moveDir = transform.TransformDirection(moveDir);
+                }
                 
-                if (Input.GetKey(KeyCode.Space)) {
-                    moveDir *= 2;
-                    animator.SetInteger("animation", 2);
+                // walk backwards
+                if (Input.GetKey(KeyCode.DownArrow)) {
+                    animator.SetInteger("animation", 1);
+                    moveDir = new Vector3(0, 0, -1);
+                    moveDir *= moveSpeed;
+                    moveDir = transform.TransformDirection(moveDir);
                 }
 
-                moveDir = transform.TransformDirection(moveDir);
+                // rotate if pressing left/right
+                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) {
+                    float runMultiply = 1;
+                    if (Input.GetKey(KeyCode.X)) {
+                        runMultiply = 1.5f;
+                    }
+                    else {
+                        animator.SetInteger("animation", 1);
+                    }
+                    rot += Input.GetAxis("Horizontal") * rotSpeed * runMultiply * Time.deltaTime;
+                    transform.eulerAngles = new Vector3(0, rot, 0);
+                }
             }
-            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) {
+
+            // return to standstill
+            if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow)
+            || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)
+            || !selected) {
                 animator.SetInteger("animation", 0);
                 moveDir = Vector3.zero;
             }
-            
-            if (controller.isGrounded) {
-                if (Input.GetKey(KeyCode.Z)) {
-                    var jumpForce = new Vector3(0, 1, 0);
-                    jumpForce *= jumpSpeed;
-                    jumpForce = transform.TransformDirection(jumpForce);
-                    controller.Move(jumpForce * Time.deltaTime);
-                }
-            }
-            
-
-            rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-            transform.eulerAngles = new Vector3(0, rot, 0);
 
             moveDir.y -= gravity * Time.deltaTime;
             controller.Move(moveDir * Time.deltaTime);
